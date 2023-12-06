ede9ce99965d57dd7981bc3f45473387295afff4
DelisG <delisemarciano@gmail.com>
2023-12-06 00:07:43 -0300

    🧹 Refatoração

--- src/components/Start.js
+++ src/components/Start.js
@@ -12,12 +12,7 @@ const Start = () => {
     >
       <div className="container">
         <div className="row vh-100 align-items-center justify-content-center">
-          <div className="col-lg-10 h-100">
-            {/* <h1 className="display-5 text-warning">
-              Inclusion for Blind People
-            </h1>
-            <small className="text-secondary">Inclusão para Cegos</small> */}
-
+          <div className="col-lg-10 h-100 d-flex justif-content-center flex-column">
             <div className="position-relative text-center px-5 pt-3">
               <img
                 src={bgmd}
@@ -29,19 +24,36 @@ const Start = () => {
                 alt="bg de mulher cega com um cachorro"
                 className="img-fluid d-none d-md-block w-100"
               />
-              <button
-                onClick={startQuiz}
-                className="btn btn-primary py-3 px-4 rounded-circle border border-primary shadow fw-bold position-absolute"
-                style={{
-                  top: "68%",
-                  left: "60%",
-                  // transform: "translate(-50%, -50%)",
-                }}
-              >
-                Start <br/> Quiz
+              <button onClick={startQuiz} className="btn">
+                <span
+                  className="d-none d-md-block position-absolute btn btn-primary py-3 px-4 rounded-circle border border-primary shadow fw-bold "
+                  style={{
+                    top: "68%",
+                    left: "60%",
+                    // transform: "translate(-50%, -50%)",
+                  }}
+                >
+                  Start <br /> Quiz
+                </span>
+                <span className="d-block d-md-none btn btn-primary py-3 px-4 shadow fw-bold ">
+                  Start Quiz
+                </span>
               </button>
             </div>
           </div>
+        </div>
+        <div className="bg-dark pb-2">
+          <p>
+            Equipe:
+            <small className="text-secondary"> Bruna | </small>
+            <small className="text-secondary">Delis | </small>
+            <small className="text-secondary">Erika | </small>
+            <small className="text-secondary">Giovanna</small>
+          </p>
+          <p>
+            Teacher:
+            <small className="text-secondary"> Andreza</small>
+          </p>
         </div>
       </div>
     </section>
