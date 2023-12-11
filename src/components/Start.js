93e46369344fc4f38f6e0a7b10a5592d5fe8d0e3
DelisG <delisemarciano@gmail.com>
2023-12-11 17:23:59 -0300

    :broom: Limpeza de código

--- src/components/Start.js
+++ src/components/Start.js
@@ -7,53 +7,40 @@ const Start = () => {
   const { startQuiz, showStart } = useContext(DataContext);
   return (
     <section
-      className="text-white text-center bg-dark"
+      className="text-white text-center bg-dark vh-100"
       style={{ display: `${showStart ? "block" : "none"}` }}
     >
       <div className="container">
-        <div className=" vh-100 d-flex justify-content-center flex-column">
-            <div className="position-relative text-center px-5 pt-3">
-              <img
-                src={bgmd}
-                alt="bg de mulher cega com um cachorro"
-                className="img-fluid d-block d-md-none"
-              />
-              <img
-                src={bg}
-                alt="bg de mulher cega com um cachorro"
-                className="img-fluid d-none d-md-block w-100"
-              />
-              <button onClick={startQuiz} className="btn">
-                <span
-                  className="d-none d-md-block position-absolute btn btn-primary py-3 px-4 rounded-circle border border-primary shadow fw-bold "
-                  style={{
-                    top: "68%",
-                    left: "60%",
-                    // transform: "translate(-50%, -50%)",
-                  }}
-                >
-                  Start <br /> Quiz
-                </span>
-                <span className="d-block d-md-none btn btn-primary py-3 px-4 shadow fw-bold ">
-                  Start Quiz
-                </span>
-              </button>
-              <div className="bg-dark pb-2">
-                <p>
-                  Equipe:
-                  <small className="text-secondary"> Bruna | </small>
-                  <small className="text-secondary">Delis | </small>
-                  <small className="text-secondary">Erika | </small>
-                  <small className="text-secondary">Giovanna</small>
-                </p>
-                <p>
-                  Teacher:
-                  <small className="text-secondary"> Andreza</small>
-                </p>
-              </div>
-            </div>
+        <div className="row d-flex justify-content-center align-items-center">
+          <div className="col-lg-10 px-5 pt-3 d-flex justify-content-center flex-column">
+            <img
+              src={bgmd}
+              alt="bg de mulher cega com um cachorro"
+              className="img-fluid d-block d-md-none"
+            />
+            <img
+              src={bg}
+              alt="bg de mulher cega com um cachorro"
+              className="img-fluid d-none d-md-block"
+            />
+            <button onClick={startQuiz} className="btn">
+              <span
+                className="d-none d-md-block position-absolute btn btn-primary py-3 px-4 rounded-circle border border-primary shadow fw-bold "
+                style={{
+                  top: "68%",
+                  left: "60%",
+                  // transform: "translate(-50%, -50%)",
+                }}
+              >
+                Start <br /> Quiz
+              </span>
+              <span className="d-block d-md-none btn btn-primary py-3 px-4 shadow fw-bold ">
+                Start Quiz
+              </span>
+            </button>       
           </div>
         </div>
+      </div>
     </section>
   );
 };
